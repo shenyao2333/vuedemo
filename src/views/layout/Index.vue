b947bed0ed78490f787924875e9b786b8c8ea510
shenyao2333 <shenyao2333@163.com>
2020-06-08 22:07:05 +0800

    添加几个破页面

---
+++ src/views/layout/Index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="loyout">
+    <el-container>
+      <layoutAside></layoutAside>
+      <el-container>
+        <layoutHeader></layoutHeader>
+        <el-main id="elmain">
+          <transition name="main" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </el-main>
+        <el-footer>
+          <Bottom></Bottom>
+        </el-footer>
+      </el-container>
+    </el-container>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Index.vue'
+}
+</script>
+
+<style  lang="scss">
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
+
+  body {
+    background-color: #f2f2f2;
+    font-size: 14px;
+    color: #333333;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  $top: top;
+  $bottom: bottom;
+  $left: left;
+  $right: right;
+  $leftright: ($left, $right);
+  %w100 {
+     width: 100%;
+   }
+
+  %h100 {
+     height: 100%;
+   }
+
+  %cursor {
+     cursor: pointer;
+   }
+
+  html, body, #loyout, .el-container, #asideNav, ul.el-menu {
+  @extend %h100;
+  }
+
+  @mixin set-value($side, $value) {
+    @each $prop in $leftright {
+      #{$side}-#{$prop}: $value;
+    }
+  }
+
+  #elmain {
+    background-color: #f0f2f5;
+  }
+
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9 !important;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+
+</style>
---
+++ src/views/layout/aside/Aside.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <el-aside id="asideNav">
+        <div class="logo-name">
+          <p v-if="$store.getters.logoShow">XU</p>
+          <p v-else>vue-xuAdmin后台模板</p>
+        </div>
+        <el-menu :default-active="$route.path" class="el-menu-vertical"
+                 @select="selectmenu"
+                 :collapse="$store.getters.isCollapse"
+                 background-color="#03152A"
+                 text-color="rgba(255,255,255,.7)"
+                 active-text-color="#ffffff"
+                 :router="$store.getters.uniquerouter"
+                 :unique-opened="$store.getters.uniquerouter"
+                 :collapse-transition="true"
+        >
+          <template v-for="(item,index) in $store.getters.routers" v-if="!item.hidden">
+            <el-submenu v-if="!item.alone && item.children.length>0" :index="index+''" :key="index">
+              <template slot="title">
+                <i :class="item.iconCls?item.iconCls:[fa,fa-server]"></i>
+                <span slot="title">{{ $t(`routeName.${item.name}`) }}</span>
+              </template>
+
+              <menu-tree :menuData="item.children"></menu-tree>
+
+            </el-submenu>
+            <el-menu-item :index="item.path" v-else :key="item.path">
+              <i :class="item.iconCls?item.iconCls:[fa,fa-file]"/>
+              <span slot="title">{{ $t(`routeName.${item.name}`) }}</span>
+            </el-menu-item>
+          </template>
+
+        </el-menu>
+      </el-aside>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Aside"
+    }
+</script>
+
+<style scoped>
+
+</style>
