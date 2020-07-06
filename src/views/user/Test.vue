cf9940afd70359383b97b8bd23245c7f830157cc
shenyao2333 <shenyao2333@163.com>
2020-07-06 22:19:03 +0800

    es6练习

--- src/views/user/Test.vue
+++ src/views/user/Test.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2 @click="test">点击事件</h2>
+    <h2 @click="test">map测试打印</h2>
+    <h2 @click="test2">List转化map</h2>
+    <h2 @click="test3">set打印</h2>
+    <h2 @click="test4">set去重打印</h2>
   </div>
 
 </template>
@@ -16,8 +19,33 @@
       }
     },
     methods: {
+
       test () {
-        console.log(a);
+        var map = new Map();
+        map.set(1,"你好");
+        map.set(2,"他好");
+        for (var[key,val] of map){
+          console.log(key + "--->"+ val);
+        }
+      },
+      test2(){
+        var list = [["key1", "value1"], ["key2", "value2"]];
+        var map = new Map(list);
+        map.forEach(function (value, key) {
+          console.log(key + " = " + value);
+        },map);
+      },
+      test3(){
+        var set = new Set(["12","13","14"]);
+        for (var a of set){
+          console.log(a);
+        }
+      },
+      test4(){
+        var mySet = new Set([1, 2, 3, 4, 4]);
+        for (var a of mySet){
+          console.log(a);
+        }
       }
     }
   }
